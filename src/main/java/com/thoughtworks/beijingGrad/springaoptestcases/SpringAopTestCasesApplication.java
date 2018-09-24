c0d190275c09eb8ae9a3c0821de46c234c3484f0
mingwhsun <mingwhsun@thoughtworks.com>
2018-09-24 14:26:55 +0800

    [sunming] init the project

---
+++ src/main/java/com/thoughtworks/beijingGrad/springaoptestcases/SpringAopTestCasesApplication.java
@@ -0,0 +1,12 @@
+package com.thoughtworks.beijingGrad.springaoptestcases;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringAopTestCasesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringAopTestCasesApplication.class, args);
+	}
+}
